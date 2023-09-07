6337dfef9e8d6e38953b8f5c2c0f51e501403011
Sohyun <so405@naver.com>
2023-09-07 14:10:29 +0900

    [FEAT] : Home 화면 구성

--- shoppi/app/src/main/java/com/shoppi_app/HomeFragment.kt
+++ shoppi/app/src/main/java/com/shoppi_app/HomeFragment.kt
@@ -5,24 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
-class HomeFragment:Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home,container,false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-           findNavController().navigate(R.id.action_home_to_product_detail)
+
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        if(!homeData.isNullOrEmpty()){
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+            val titleValue = Title(text, iconUrl)
+            toolbarTitle.text = titleValue.text
+            GlideApp.with(this).load(titleValue.iconUrl).into(toolbarIcon)
         }
 
     }
